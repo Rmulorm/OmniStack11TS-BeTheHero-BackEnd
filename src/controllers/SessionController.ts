3c369fba2106e2f01ca76307525c7e4a0587d9b7
Rômulo Maciel <romulo.maciel@sap.com>
2020-03-29 14:47:42 -0300

    controllers(Session): now receives ngoId via body

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -4,17 +4,17 @@ import connection from '../database/connection';
 
 export default {
   async create(request: Request, response: Response) {
-    const ngo_id = request.headers.authorization;
+    const { id } = request.body;
 
-    const ngo_name = await connection('ngos')
-      .where('id', ngo_id)
+    const ngo = await connection('ngos')
+      .where('id', id)
       .select('name')
       .first();
 
-    if (!ngo_name) {
+    if (!ngo) {
       return response.status(400).json({ error: 'No NGO Found with this ID' });
     }
 
-    return response.json({ name: ngo_name });
+    return response.json( ngo );
   }
 }
